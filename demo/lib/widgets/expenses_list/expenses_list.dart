878060f48c3100e4912f6bcbed220981fec2a87c
Ritu <mangorerituja@gmail.com>
2024-05-23 21:59:35 -0700

    Grid view added on the first screen to choose categories and list view added on the second screen to show menus of that category.

---
+++ demo/lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../models/expense.dart';
+import 'expense_item.dart';
+
+class ExpensesList extends StatelessWidget{
+  const ExpensesList ({super.key, required this.expenses, required this.onRemoveExpense,});
+
+  final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
+
+  @override
+  Widget build(BuildContext context) {
+   return ListView.builder(itemCount: expenses.length,itemBuilder: (ctx,index) =>
+       Dismissible(background: Container(color: Theme.of(context).colorScheme.error,
+       margin: const EdgeInsets.symmetric(horizontal: 16),
+       ),
+           key: ValueKey(expenses[index]),
+       onDismissed: (direction) {
+         onRemoveExpense(expenses[index]);
+       },
+       child:ExpenseItem(expenses[index])));
+  }
+}
+
+
